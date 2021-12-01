3fd42312725b455dbc62bb5c66aa1e25064da59c
Amine Smahi <md.amine.smahi@gmail.com>
2021-12-01 18:23:46 +0100

    Setup routes, create controllers, create view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/', :to => 'users#index'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+
 end
