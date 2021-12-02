e7f6a9443cf41cc38fa8e16f396aed4a72a5cc57
Amine Smahi <md.amine.smahi@gmail.com>
2021-12-02 12:20:41 +0100

    Write tests for controllers

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    before(:example) { get users_path }
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+
+    it 'has a text in the view' do
+      expect(response.body).to include('Find me in app/views/users/index.html.erb')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:example) { get '/users/show' }
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'show' template" do
+      expect(response).to render_template('show')
+    end
+
+    it 'has a text in the view' do
+      expect(response.body).to include('Find me in app/views/users/show.html.erb')
+    end
+  end
+end
