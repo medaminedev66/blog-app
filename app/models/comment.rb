8f7f2729fbd3fee04fbe7708c3bb96c9ff3aeb06
Amine Smahi <md.amine.smahi@gmail.com>
2021-12-06 11:42:44 +0100

    Set up associations between models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+  belongs_to :user, index: true, foreign_key: true
+  belongs_to :post, index: true, foreign_key: true
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,4 @@
 class Like < ApplicationRecord
+  belongs_to :user, index: true, foreign_key: true
+  belongs_to :post, index: true, foreign_key: true
 end
