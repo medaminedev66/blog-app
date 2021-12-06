4c3eac17765a175bff9e34d0e3633cd51f60f87d
medaminedev66 <md.amine.smahi@gmail.com>
2021-12-06 15:23:46 +0100

    Add recent_posts method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
-    has_many :comments
-    has_many :posts
-    has_many :likes
+    has_many :comments, foreign_key: "Author_id", class_name: "Comment"
+    has_many :posts, foreign_key: "Author_id", class_name: "Post"
+    has_many :likes, foreign_key: "Author_id", class_name: "Like"
 
     def recent_posts
+      recent_posts_array = []
+      i = 0
       self.posts.each do |post|
-        puts "Post title #{post.Title}"
+        break if i > 3
+        recent_posts_array << post
+        i += 1
       end
+    return recent_posts_array
     end
 end
