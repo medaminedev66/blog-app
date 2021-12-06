0454f977f6d6b82a04dfd9311d8a26b0c6b75709
Amine Smahi <md.amine.smahi@gmail.com>
2021-12-06 16:57:27 +0100

    Create update_counter method in Post class to update the postCounters of the author

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
     belongs_to :Author_id, class_name: 'User', primary_key: :id, foreign_key: :Author_id
     has_many :likes
     has_many :comments
+
+    def update_counter
+      if self.Author_id.PostsCounter == nil
+         self.Author_id.update(PostsCounter: 1)
+      else
+        self.Author_id.PostsCounter+=1
+        self.Author_id.update(PostsCounter: self.Author_id.PostsCounter)
+      end
+    end
 end
