6c9a3071280ded3aa9661b9e9f0fc9cdfc9c25b8
medaminedev66 <md.amine.smahi@gmail.com>
2021-12-10 23:45:44 +0100

    remove comments

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -10,12 +10,10 @@ RSpec.describe Like, type: :model do
   end
 
   it 'update the likes counter of post' do
-    # subject.update_likes_counter
     expect(subject.post_id).to eq(7)
   end
 
   it 'checks that the post_id is integer' do
-    # subject.update_likes_counter
     subject.post_id = 7.5
     expect(subject).to_not be_valid
   end
