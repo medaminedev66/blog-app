18b9164bce556de21c6ff60cf87da373b5aad1f9
medaminedev66 <md.amine.smahi@gmail.com>
2021-12-10 21:25:31 +0100

    Make sure that the N+1 problem is solved when fetching all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find_by(id: params[:user_id])
-    @posts = Post.all.where(author_id: params[:user_id])
+    @user = User.includes(:posts).find(params[:user_id])
   end
 
   def show
     @user = User.find_by(id: params[:user_id])
-    @post = Post.find_by(id: params[:id], author_id: params[:user_id])
+    @post = @user.posts.includes(:comments, :likes).find(params[:id])
     @comments = Comment.all.where(post_id: params[:id])
   end
 
