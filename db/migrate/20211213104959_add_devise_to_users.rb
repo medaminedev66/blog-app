949c4b52970b8d29475b2ea156c87f457b241d6a
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-13 15:34:45 +0100

    Add Confirmation Token Index To Users Table

--- db/migrate/20211213104959_add_devise_to_users.rb
+++ db/migrate/20211213104959_add_devise_to_users.rb
@@ -6,7 +6,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -39,7 +39,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 
