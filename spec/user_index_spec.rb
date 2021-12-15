c57257c6c83ec793036bfaa44883a9727485b010
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-15 19:00:23 +0100

    Add More Tests For Users Index Action

--- spec/user_index_spec.rb
+++ spec/user_index_spec.rb
@@ -4,16 +4,21 @@ RSpec.describe "users/index.html.erb", type: :feature do
   before(:each) do
     visit users_path
     @user1 = User.new(Name: "Amine", Bio: "Full stack developer", email: "md.amine.smahi@gmail.com", password: "admin@2020", Photo: "photo_url", PostsCounter: 3, confirmed_at: Time.now)
+    @user2 = User.new(Name: "Dammy", Bio: "Full stack developer", email: "dammy@ymail.com", password: "admin@2040", Photo: "no_img.png", PostsCounter: 0, confirmed_at: Time.now)
   end
 
   it "checks name of all users" do
     @user1.save
-    expect(page).to have_content('Amine')
+    @user2.save
+    expect(page).to have_content @user1.name
+    expect(page).to have_content @user2.name
   end
 
   it "checks photo of all users" do
     @user1.save
-    expect(page).to have_content 'photo_url'
+    @user2.save
+    expect(page).to have_content @user1.photo
+    expect(page).to have_content @user2.photo
   end
 
   it "checks posts of all users" do
